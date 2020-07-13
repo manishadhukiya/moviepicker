94036b183d535a4b540a06a45f6011efaa9480b6
Manisha <manishadhukiya14@gmail.com>
2020-07-13 19:46:52 +0530

    API - Input and Output

--- routes/backend.js
+++ routes/backend.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/pickMovies', function(req, res, next) {
+  var inputMovies = req.body.movies;
+
+  console.log(inputMovies);
+
+  var outputMovies = inputMovies;
+
+  return res.json({ "status": "success", "message": "Movies Picked!", "movies": outputMovies });
 });
 
 module.exports = router;
