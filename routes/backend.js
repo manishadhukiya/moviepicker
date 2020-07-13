a36ed71368e50c2ccf7c12da9c05b80fe48592d6
Manisha <manishadhukiya14@gmail.com>
2020-07-13 20:19:11 +0530

    Converting date to day of the year

--- routes/backend.js
+++ routes/backend.js
@@ -3,13 +3,35 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/pickMovies', function(req, res, next) {
-  var inputMovies = req.body.movies;
+	var inputMovies = req.body.movies;
 
-  console.log(inputMovies);
+	var outputMovies = [];
 
-  var outputMovies = inputMovies;
+	var l = inputMovies.length;
 
-  return res.json({ "status": "success", "message": "Movies Picked!", "movies": outputMovies });
+	for(var i = 0; i < l; i++) {
+		var movie = inputMovies[i];
+
+		var oneDay = 1000 * 60 * 60 * 24;
+
+		var now = new Date(movie.start);
+		var yearStart = new Date(now.getFullYear(), 0, 0);
+		var diff = (now - yearStart);
+		var day = Math.floor(diff / oneDay);
+		
+		movie.startd = day;
+
+		now = new Date(movie.end);
+		yearStart = new Date(now.getFullYear(), 0, 0);
+		diff = (now - yearStart);
+		day = Math.floor(diff / oneDay);
+
+		movie.endd = day;
+
+		outputMovies.push(movie);
+	}
+
+	return res.json({ "status": "success", "message": "Movies Picked!", "movies": outputMovies });
 });
 
 module.exports = router;
